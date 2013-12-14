2e4a1c25d7bc9e346bf0551bf67a0cb3514dd00f
Nick Arnoeyts <nickstories@gmail.com>
2013-12-14 13:21:33 +0100

    added player input

--- Source/Main.hx
+++ Source/Main.hx
@@ -5,6 +5,7 @@ import flash.display.Sprite;
 import flash.events.KeyboardEvent;
 import flash.ui.Keyboard;
 import objects.Entity;
+import objects.Player;
 
 import motion.easing.Elastic;
 import motion.easing.Bounce;
@@ -13,17 +14,17 @@ import motion.easing.Quad;
 
 import motion.Actuate;
 
-import objects.Input;
-
-
 
 class Main extends Sprite {
 	
+	static var player:Player;
 	
 	public function new () {
 		
 		super ();
 		
+		player = new Player(15);
+		addChild(player);
 		
 		var blok1 = new Entity("redblock.png", 25, 25);
 		var blok2 = new Entity("redblock.png", 240, 25);
@@ -49,18 +50,24 @@ class Main extends Sprite {
 	
 	static function onKeyDown (event:KeyboardEvent)
 	{
-		Input.registerKeyState(event.keyCode, true);
-		Input.registerKeyState(Keyboard.CONTROL, event.ctrlKey);
-		Input.registerKeyState(Keyboard.ALTERNATE, event.altKey);
-		Input.registerKeyState(Keyboard.SHIFT, event.shiftKey);
+		if (player == null) return;
+		
+		switch (event.keyCode) 
+		{
+			case Keyboard.RIGHT:
+				player.GoRight();
+			case Keyboard.LEFT:
+				player.GoLeft();
+			case Keyboard.UP:
+				player.GoUp();
+			case Keyboard.DOWN:
+				player.GoDown();
+		}
 	}
 	
 	static function onKeyUp (event:KeyboardEvent)
 	{
-		Input.registerKeyState(event.keyCode, false);
-		Input.registerKeyState(Keyboard.CONTROL, event.ctrlKey);
-		Input.registerKeyState(Keyboard.ALTERNATE, event.altKey);
-		Input.registerKeyState(Keyboard.SHIFT, event.shiftKey);
+		
 	}
 	
 	
