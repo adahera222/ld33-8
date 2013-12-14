99cdcdad274bd0e1237a1bc0ef0c32d61d49e8f7
darkking2005 <darkking@live.com>
2013-12-14 12:14:59 +0100

    Animation tests

--- Source/Main.hx
+++ Source/Main.hx
@@ -3,6 +3,12 @@ package;
 
 import flash.display.Sprite;
 import objects.Entity;
+import motion.easing.Elastic;
+import motion.easing.Bounce;
+import motion.easing.Cubic;
+import motion.easing.Quad;
+
+import motion.Actuate;
 
 
 class Main extends Sprite {
@@ -12,10 +18,20 @@ class Main extends Sprite {
 		
 		super ();
 		
-		addChild(new Entity("redblock.png", 25, 25));
-		addChild(new Entity("redblock.png", 240, 25));
-		addChild(new Entity("redblock.png", 25, 240));
-		addChild(new Entity("redblock.png", 240, 240));
+		var blok1 = new Entity("redblock.png", 25, 25);
+		var blok2 = new Entity("redblock.png", 240, 25);
+		var blok3 = new Entity("redblock.png", 25, 240);
+		var blok4 = new Entity("redblock.png", 240, 240);
+		
+		addChild(blok1);
+		addChild(blok2);
+		addChild(blok3);
+		addChild(blok4);
+		
+		Actuate.tween(blok1, 3, { x: 300, y: 300 } ).delay(0.4).ease(Bounce.easeOut);
+		Actuate.tween(blok2, 3, { x: 400, y: 200 } ).delay(0.4).ease(Elastic.easeIn);
+		Actuate.tween(blok3, 3, { x: 50, y: 3 } ).delay(0.4).ease(Cubic.easeInOut);
+		Actuate.tween(blok4, 3, { x: 100, y: 50 } ).delay(0.4).ease(Quad.easeOut);
 		
 	}
 	
